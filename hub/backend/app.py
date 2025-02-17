28ddeaec22ce1fef8c8155cb255de5058694e2e8
erparks <erparks24@gmail.com>
2025-02-17 13:48:52 -0700

    periodic reporting for moisture sensors thru wifi

--- hub/backend/app.py
+++ hub/backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
@@ -6,9 +6,18 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/api/data")
+@app.route("/api/data", methods=['POST'])
 def get_data():
-    return jsonify({"message": "Hello from Flask!"})
+    try:
+        data = request.get_json()
+        sensor_type = data.get("sensor")
+        sensor_value = data.get("value")
+
+        print(f"Received Data -> Sensor: {sensor_type}, Value: {sensor_value}")
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+    return jsonify({"message": "thanks!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
