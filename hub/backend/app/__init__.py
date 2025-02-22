b1e679c7f6e81580329a4e9d4be7d252e5524a44
erparks <erparks24@gmail.com>
2025-02-22 12:25:44 -0700

    request to pi from pc

--- hub/backend/app/__init__.py
+++ hub/backend/app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from app.database import db, migrate
 from app.config import Config
 from app.routes.plants import plants_bp
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    CORS(app)
+    
     # Initialize database
     db.init_app(app)
     migrate.init_app(app, db)
